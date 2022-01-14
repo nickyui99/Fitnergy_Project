662b1880f7b15c35f2d192fb778c9927d916cbe2
Narresh <narreshnaidu@gmail.com>
2022-01-14 17:35:34 +0800

    commit 1

--- app/src/main/java/com/rtn/fitnergy/social/SocialFragment.java
+++ app/src/main/java/com/rtn/fitnergy/social/SocialFragment.java
@@ -22,7 +22,7 @@ public class SocialFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-//comment
+//comment1
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
